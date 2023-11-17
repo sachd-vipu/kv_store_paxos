0c28b141509d97b4ce582e99bfee5d4de644d5d7
Vipul Sachdeva <sachd.vipu@gmail.com>
2023-11-17 14:57:23 -0500

    define common constants

--- src/kvpaxos/common.go
+++ src/kvpaxos/common.go
@@ -1,8 +1,13 @@
 package kvpaxos
 
 const (
-	OK       = "OK"
-	ErrNoKey = "ErrNoKey"
+	OK           = "OK"
+	Get          = "Get"
+	Put          = "Put"
+	Append       = "Append"
+	ErrNoKey     = "ErrNoKey"
+	ErrPending   = "ErrPending"
+	ErrForgotten = "ErrForgotten"
 )
 
 type Err string
@@ -16,6 +21,7 @@ type PutAppendArgs struct {
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	SeqNo int64
 }
 
 type PutAppendReply struct {
@@ -25,6 +31,7 @@ type PutAppendReply struct {
 type GetArgs struct {
 	Key string
 	// You'll have to add definitions here.
+	SeqNo int64
 }
 
 type GetReply struct {
